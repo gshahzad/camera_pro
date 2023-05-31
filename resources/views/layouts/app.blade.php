36fbfc13c8e923498465ba20e75b48ab4643b8c2
Najam <56229323+najam16528@users.noreply.github.com>
2023-05-31 10:41:19 +0100

    favicon and navigation class update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,12 @@
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
     <link rel="stylesheet" href="{{ asset('css/style.css')}}" />
     @livewireStyles
+    
+    <link rel="apple-touch-icon" href="{{asset('img/apple-touch-icon.png')}}">
+<link rel="icon" type="image/png"  href="{{asset('img/favicon-32x32.png')}}">
+<link rel="icon" type="image/png"  href="{{asset('img/favicon-16x16.png')}}">
+<link rel="manifest" href="{{asset('img/site.webmanifest')}}">
+    
 </head>
 <body>
 	<x-header></x-header>
